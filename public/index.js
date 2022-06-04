6515d5fe663ca06f5f62e7c652d178bdefcb3ec0
Ryan Wendell <Ryan.wendell@me.com>
2022-06-03 23:48:43 -0700

    done

--- public/index.js
+++ public/index.js
@@ -4,6 +4,90 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    
+    let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=f9c478e43f3941f4b1987f806f5e29e8')
+    let result = await response.json()
+    
+    const { GME, MSFT, DIS, BNTX } = result;
+    const stocks = [GME, MSFT, DIS, BNTX];
+    console.log(stocks)
+
+
+    stocks.forEach( stock => stock.values.reverse())
+    // Time Chart
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.reverse().map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.reverse().map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
+    
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Highest',
+                data: stocks.map(stock => getHighest(stock)),
+                backgroundColor: stocks.map(stock => getColor(stock.meta.symbol)),
+                borderColor: stocks.map(stock => getColor(stock.meta.symbol)),
+            }]
+        }
+    });
+    new Chart(averagePriceChartCanvas.getContext('2d'), {
+        type: 'pie',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                data: stocks.map(stock => parseFloat(getAverage(stock))),
+                backgroundColor: stocks.map(stock => getColor(stock.meta.symbol)),
+                borderColor: stocks.map(stock => getColor(stock.meta.symbol)),
+            }]
+        }
+    });
+
+    
 }
 
 main()
+
+function getAverage(stock){
+    let average = 0;
+    for(let i = 0; i < stock.values.length; i++){
+        average += stock.values[i].high
+    }
+    console.log(average/stock.values.length)
+    return average 
+
+}
+
+function getHighest(stock){
+    let highestIndex = 0;
+    for(let i = 0; i < stock.values.length; i++){
+        if(stock.values[highestIndex].high < stock.values[i].high){
+            highestIndex = i
+        }
+    }
+    return stock.values[highestIndex].high
+}
+
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
